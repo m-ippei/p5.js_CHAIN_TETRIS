5cd8a76c947dc37db942ad518a28330e05ac2e73
m-ippei <31976878+m-ippei@users.noreply.github.com>
2018-05-30 17:44:55 +0900

    Update deletes.js

--- archive/deletes.js
+++ archive/deletes.js
@@ -18,9 +18,24 @@ var Field = [
 	[9,0,0,0,0,0,0,0,0,0,0,9],
 	[9,0,0,0,0,0,0,0,0,0,0,9],
 	[9,0,0,0,0,0,0,0,0,0,0,9],
-	[9,"#","#","#","#","#","#","#","#","#","#",9],
+	[9,0,0,0,0,0,0,0,0,0,0,9],
+	[9,0,0,0,0,0,0,0,0,0,0,9],
 	[9,9,9,9,9,9,9,9,9,9,9,9],
 ]
+
+function setup() {
+  createCanvas(120, 220);
+	frameRate(1)
+}
+
+function draw() {
+  background(220);
+	deleteLines()
+}
+
+function deleteLine(){
+	arr = deleteLines()
+}
 
 function Delete(num) {
 	var arr = Field.slice(0,num)
@@ -30,11 +45,36 @@ function Delete(num) {
 	Field = arr
 }
 
-function setup() {
-  createCanvas(400, 400);
-	Delete(19)
+function checkArr(arr) {
+	for(var i = 0; i < 11;i++){
+		if(arr[i] === false){
+			return false
+		}
+	}
+	return true
 }
 
-function draw() {
-  background(220);
+function doDelete(arr) {
+	for(var i = 1; i<=arr.length;i++){
+		Delete(arr[i])
+	}
 }
+
+function deleteLines(){
+	var queueDeleteArr = []
+	for(var i=0;i<22;i++){
+		var arr = [false,false,false,false,false,false,false,false,false,false]
+		for(var j=1;j<11;j++){
+			if(typeof Field[i][j] === "string"){
+				arr[j] = true
+			}
+		}
+		if(checkArr(arr)){
+			append(queueDeleteArr,i)
+		}
+	}
+	doDelete(queueDeleteArr)
+}
+
+
+
